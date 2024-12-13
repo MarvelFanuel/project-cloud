cf2971f824d6f2140741f722154d7792e22c3088
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-12-13 12:12:34 +0700

    fix: seeder

--- database/seeders/DivisionSeeder.php
+++ database/seeders/DivisionSeeder.php
@@ -16,35 +16,42 @@ class DivisionSeeder extends Seeder
             [
                 'name' => 'Information Technology',
                 'slug' => 'it',
-                'project_link' => 'abcde',
+                'project_link' => 'https://youtube.com'
             ],
             [
                 'name' => 'Creative',
                 'slug' => 'creative',
+                'project_link' => 'https://youtube.com'
             ],
             [
                 'name' => 'Keamanan',
                 'slug' => 'keamanan',
+                'project_link' => 'https://youtube.com'
             ],
             [
                 'name' => 'Perlengkapan',
                 'slug' => 'perkap',
+                'project_link' => 'https://youtube.com'
             ],
             [
                 'name' => 'Sekretariat',
                 'slug' => 'sekret',
+                'project_link' => 'https://youtube.com'
             ],
             [
                 'name' => 'Konsumsi',
                 'slug' => 'konsum',
+                'project_link' => 'https://youtube.com'
             ],
             [
                 'name' => 'Kesehatan',
                 'slug' => 'kesehatan',
+                'project_link' => 'https://youtube.com'
             ],
             [
                 'name' => 'Acara',
                 'slug' => 'acara',
+                'project_link' => 'https://youtube.com'
             ],
         ];
         foreach ($divisions as $division) {
