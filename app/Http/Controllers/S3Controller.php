02bf126814404d6a66e84d093073aaea0e3a862b
Jeffry Hermawan <jeffryhermawantm@gmail.com>
2024-12-13 13:36:34 +0700

    fix: user update slug

--- app/Http/Controllers/S3Controller.php
+++ app/Http/Controllers/S3Controller.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -12,17 +14,19 @@ class S3Controller extends Controller
         $request->validate([
             $slug => 'required|file|mimes:jpg,png,jpeg,pdf|max:5120',  // 5MB max
         ]);
-    
+
         if ($request->hasFile($slug)) {
             $file = $request->file($slug);
             $fileName = time() . '_' . $file->getClientOriginalName();
-            
+
             try {
                 // Save file to S3
                 $path = $file->storePubliclyAs('uploads', $fileName, 's3');
-                $path = $file->storePubliclyAs('uploads/'. $slug, $fileName, 'public');
+                $path = $file->storePubliclyAs('uploads/' . $slug, $fileName, 'public');
+                $user = User::where('email', session('email'))->first();
+                $user->update([$slug => $path]);
                 $fileUrl = Storage::disk('s3')->url($path);
-    
+
                 return response()->json([
                     'success' => true,
                     'message' => 'File uploaded successfully!',
@@ -36,8 +40,7 @@ class S3Controller extends Controller
                 ], 500);
             }
         }
-    
+
         return response()->json(['success' => false, 'message' => 'No file uploaded'], 400);
     }
-    
 }
